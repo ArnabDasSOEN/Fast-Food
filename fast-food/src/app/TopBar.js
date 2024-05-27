b81b3ace86654858d52a2f3cf7dd475d78058bfd
Arnab Das <arnabdas1234@hotmail.com>
2024-05-27 18:39:06 -0400

    CSS + minor change to component

--- fast-food/src/app/TopBar.js
+++ fast-food/src/app/TopBar.js
@@ -1,14 +1,16 @@
 import "./css/TopBar.css";
 function TopBar(){
 
+    // #131212 -> color for the borders of buttons
 return (
     <section className="topBar">
-        <button id="sideBar">side bar</button>
-        <h2 id="logo">Fast food!</h2>
-        <div id="importantButtons">
-            <button id="login">login</button>
-            <button id="history">history</button> 
-        </div>
+        
+            <button id="sideBarButton">side bar</button>
+            <h2 id="logo">Fast-food!</h2>
+            <div id="importantButtons">
+                <button id="login">login</button>
+                <button id="history">history</button> 
+            </div>
         
         
     </section>
