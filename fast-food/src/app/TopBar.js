6963db70ec4066e0c02cd9b9d2b6691744667568
Arnab Das <arnabdas1234@hotmail.com>
2024-05-27 14:04:44 -0400

    created top Bar
    will have to work on css later on, but main focus of project is functionality so leaving it as is

---
+++ fast-food/src/app/TopBar.js
@@ -0,0 +1,19 @@
+import "./css/TopBar.css";
+function TopBar(){
+
+return (
+    <section className="topBar">
+        <button id="sideBar">side bar</button>
+        <h2 id="logo">Fast food!</h2>
+        <div id="importantButtons">
+            <button id="login">login</button>
+            <button id="history">history</button> 
+        </div>
+        
+        
+    </section>
+
+);
+}
+
+export default TopBar;
--- fast-food/src/app/page.js
+++ fast-food/src/app/page.js
@@ -1,10 +1,18 @@
 //main page run with "npm run dev" in terminal
+/* 
+main css colors:  
+    #B9E3C6
+    #FFEAEE
+    #D81E5B
+    #23395B
+    #FFFD98
+*/
 
-import RandomPokemon from "./RandomPokemon";
-
+//import RandomPokemon from "./RandomPokemon";
+import TopBar from  "./TopBar.js";
 
 export default function Home() {
   return (
-    <RandomPokemon/>
+    <TopBar/>
   );
 }
