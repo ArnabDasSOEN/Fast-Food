03907c2ab2d174722fef0568d2ec7370dc77ad0b
Arnab Das <arnabdas1234@hotmail.com>
2024-05-27 20:06:20 -0400

    added CSS to the food cards

--- fast-food/src/app/FoodCard.js
+++ fast-food/src/app/FoodCard.js
@@ -1,15 +1,16 @@
+import "./css/FoodCard.css"
+
+
+
 function FoodCard(props){
-
-
-
 //source of image will be provided as a prop through prop name "src"
 //3 props necessary: name, price and image source.
     return(
         <div className="card">
-            <img src={props.src}></img>
+            <img id="CardImage" src={props.src}></img>
             <div className="CardInfo">
                 <h4 className="NameOfFood">{props.name}</h4>
-                <h5 className="PriceOfFood">{props.price}</h5>
+                <h5 className="PriceOfFood">${props.price}</h5>
             </div>
             <div className="CardButtons">
                 <button>Buy</button>
