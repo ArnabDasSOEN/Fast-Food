3e41a293ce11f0c925a966699f24462488e44935
Arnab Das <arnabdas1234@hotmail.com>
2024-06-22 17:30:00 -0400

    fixed shopping cart
    problem with shopping cart

--- fast-food/src/app/MainSection.js
+++ fast-food/src/app/MainSection.js
@@ -10,28 +10,28 @@ import CartItem from "./CartItem.js";
 function MainSection({classname, menuItems}) {
     
     const [addedItems, setAddedItems] = useState([]);
-    const [cartItemsDisplayed, setCartItemsDisplayed] = useState(addedItems.map( (items) => {
-             return <CartItem name={items.name} price={items.price}/>;
-         }));
-    
+    const [cartItemsDisplayed, setCartItemsDisplayed] = useState([]);
+  
+
     function refreshCartItemsDisplayed(){ 
         setCartItemsDisplayed(addedItems.map( (items) => { 
             return <CartItem name={items.name} price={items.price}/>
         }));
         console.log(addedItems);
     }
-
+    
     const handleClicks = []; 
     let handleClickscounter = 0;
     const foodCardItems = menuItems.map((items) => { 
         handleClicks[handleClickscounter] = () => { 
-            setAddedItems(addedItems.push({name:items.name, price:items.price})); //try [...addedItems,{new object}]
-            refreshCartItemsDisplayed();
+            setAddedItems([...addedItems, {name:items.name, price:items.price} ]);
+            refreshCartItemsDisplayed(); 
+            console.log("Printed in handleClicks")
              }
         handleClickscounter++;
         return <FoodCard name={items.name} price={items.price} src={items.src} handler={handleClicks[handleClickscounter - 1]} />;
     });
-   
+
     return (
         <main role="Menu" className="Menu">
 
