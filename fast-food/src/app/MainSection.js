4f56581783c86fa5c491251f3dc5eecf2159b092
Arnab Das <arnabdas1234@hotmail.com>
2024-06-22 11:50:15 -0400

    TypeError attempt to fix.
    Pushing to safeguard changes

--- fast-food/src/app/MainSection.js
+++ fast-food/src/app/MainSection.js
@@ -6,19 +6,14 @@ import React, { useState } from "react";
 import CartItem from "./CartItem.js";
 
 
-    //TODO: .map TypeError fix and .push TypeError fix.
-function MainSection({classname, menuItems}) {
-    //Any component that changes dynamically during usage must be used by useState hook.
-
-    const [addedItems, setAddedItems] = useState([]);
     
-    //This is for the cart. Since its a component that displays items (which is dynamic) It needs to be put into a state in order for react to rerender it.
+function MainSection({classname, menuItems}) {
+    
+    const [addedItems, setAddedItems] = useState([]);
     const [cartItemsDisplayed, setCartItemsDisplayed] = useState(addedItems.map( (items) => {
              return <CartItem name={items.name} price={items.price}/>;
          }));
     
-    
-         //function that will rerender the cartItemsDisplayed variable above.
     function refreshCartItemsDisplayed(){ 
         setCartItemsDisplayed(addedItems.map( (items) => { 
             return <CartItem name={items.name} price={items.price}/>
@@ -26,31 +21,17 @@ function MainSection({classname, menuItems}) {
         console.log(addedItems);
     }
 
-
-    //array of functions responsible for handling the onClick event inside the FoodCards buttons
-    const handleClicks = []; //we need the functions to still exist to handle the onClick events. in other words, we need pointers to point to those functions so that they stay in heap.
-                             //Arrays are pointers. Each index is a pointer. Consequently, this problem is solved by having an array of functions that all handle the onClick events of the food
-                             //cards
+    const handleClicks = []; 
     let handleClickscounter = 0;
-    const foodCardItems = menuItems.map((items) => { //menuItems is the array passed to this component from the page.js file. We do this because the alternative is to write each foodcard and
-                                                     // handleClick events manually.
-        handleClicks[handleClickscounter] = () => { //were just creating the function, were not calling or doing anything here.
+    const foodCardItems = menuItems.map((items) => { 
+        handleClicks[handleClickscounter] = () => { 
             setAddedItems(addedItems.push({name:items.name, price:items.price})); //try [...addedItems,{new object}]
             refreshCartItemsDisplayed();
-            
-            
-            // addedItems.push({name:items.name, price:items.price}); //TODO TRY PUSH, IF NOT WORK, THEN YOU NEED TO USE USESTATE =-=--=-==-=-
-            //      setNumOfAddedItems(numOfAddedItems + 1);//this serves as the number of added items and the next index in which we need to add inside the array addedItems
-            //      refreshCartItemsDisplayed();//refreshing the items displayed in the cart.
              }
         handleClickscounter++;
         return <FoodCard name={items.name} price={items.price} src={items.src} handler={handleClicks[handleClickscounter - 1]} />;
-        //I was not aware we could do this. But essentially, you can have a looping system that returns a bunch of components, encapsulate all of this inside a variable, and then simply return
-        //that variable and it's going to return all those components.
-        //So instead of having 15 different foodcards in the return section, you can have a single variable that contains all those food cards.
     });
    
-    //when you use curly braces in the return section of a JSX component. Its to indicate that we are leaving React and entering the world of regular javascript.
     return (
         <main role="Menu" className="Menu">
 
